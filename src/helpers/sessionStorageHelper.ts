83702570fa3ff262795c0694d399541e689d22b2
anhquan202 <anhquan18012002@gmail.com>
2026-01-26 00:29:09 +0000

    update

--- src/helpers/sessionStorageHelper.ts
+++ src/helpers/sessionStorageHelper.ts
@@ -1,4 +1,6 @@
 const TOKEN_KEY = "token"
+const USER_KEY = "user"
+
 const getToken = () => {
     return sessionStorage.getItem(TOKEN_KEY)
 }
@@ -11,8 +13,24 @@ const removeToken = () => {
     sessionStorage.removeItem(TOKEN_KEY)
 }
 
+const getUser = () => {
+    const user = sessionStorage.getItem(USER_KEY)
+    return user ? JSON.parse(user) : null
+}
+
+const setUser = (user: any) => {
+    sessionStorage.setItem(USER_KEY, JSON.stringify(user))
+}
+
+const removeUser = () => {
+    sessionStorage.removeItem(USER_KEY)
+}
+
 export const sessionStorageHelper = {
     getToken,
     setToken,
-    removeToken
+    removeToken,
+    getUser,
+    setUser,
+    removeUser
 }
